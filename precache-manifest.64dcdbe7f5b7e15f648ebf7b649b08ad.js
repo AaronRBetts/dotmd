9bf851cf3a7498ad647cbb85da3f5d007f446777
AaronRBetts <aaronrbetts@gmail.com>
2020-06-09 14:00:25 +0300

    Updates

---
+++ precache-manifest.64dcdbe7f5b7e15f648ebf7b649b08ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88c328325a37f86320a9a06143c2eb5a",
+    "url": "/dotmd/index.html"
+  },
+  {
+    "revision": "a3959ae7b1e08abfd7ed",
+    "url": "/dotmd/static/css/2.ace36aee.chunk.css"
+  },
+  {
+    "revision": "1e0ebe3c67a38a7ca161",
+    "url": "/dotmd/static/css/main.ae53596f.chunk.css"
+  },
+  {
+    "revision": "a3959ae7b1e08abfd7ed",
+    "url": "/dotmd/static/js/2.f0f89248.chunk.js"
+  },
+  {
+    "revision": "0472107b6f246c1e2ca64f14170aecd1",
+    "url": "/dotmd/static/js/2.f0f89248.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e0ebe3c67a38a7ca161",
+    "url": "/dotmd/static/js/main.50cc5141.chunk.js"
+  },
+  {
+    "revision": "e724cad1854b09d5f82d",
+    "url": "/dotmd/static/js/runtime-main.e52ea879.js"
+  }
+]);
